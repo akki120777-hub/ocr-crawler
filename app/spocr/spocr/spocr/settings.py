7e32f5c51d530df8db34d012ee745b175c2e4108
akki120777-hub <akki120777@gmail.com>
2026-09-18 15:17:05 +0900

    Update settings.py

--- app/spocr/spocr/spocr/settings.py
+++ app/spocr/spocr/spocr/settings.py
@@ -22,9 +22,9 @@ ADDONS = {}
 ROBOTSTXT_OBEY = True
 
 # Concurrency and throttling settings
-#CONCURRENT_REQUESTS = 16
+CONCURRENT_REQUESTS = 8
 CONCURRENT_REQUESTS_PER_DOMAIN = 1
-DOWNLOAD_DELAY = 1
+DOWNLOAD_DELAY = 3
 
 # Disable cookies (enabled by default)
 #COOKIES_ENABLED = False
